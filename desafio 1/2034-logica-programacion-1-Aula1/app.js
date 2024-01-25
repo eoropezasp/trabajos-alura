232e48a55c3cea93f24e3220ba5ac20410e7bcc3
emmanuel <eoropezasp@gmail.com>
2024-01-25 13:05:27 -0300

    ciclos

--- desafio 1/2034-logica-programacion-1-Aula1/app.js
+++ desafio 1/2034-logica-programacion-1-Aula1/app.js
@@ -1,8 +1,10 @@
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero por favor:");
+let numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
 
 console.log(numeroUsuario);
 
 if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el numero');
+    alert(`Acertaste el numero es : ${numeroUsuario} `);
+}else{
+    alert('lo siento no acertaste')
 }
